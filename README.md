432e578acfc7a2ec7946f9a558f1ea8e6f0e5f42
Shivam Kumar <shivkumar386112@gmail.com>
2022-12-24 15:31:51 +0530

    finalized everything bugs fixed

---
+++ README.md
@@ -0,0 +1,80 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is an Interactive rating component made with HTML Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Screenshot
+#### At Desktop(rating-page):
+![](./design/desktop-design.jpg)
+#### At Desktop(thankyou-page):
+![](./design/desktop-thank-you-state.jpg)
+#### At Mobile(rating-page):
+![](./design/mobile-design.jpg)
+#### At Mobile(thankyou-page):
+![](./design/mobile-thank-you-state.jpg)
+
+### Links
+
+- Live Site URL: [Click here](https://your-live-site-url.com)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Javascript
+
+### What I learned
+
+I learned the basic mobile-first workflow and practiced the concept of flexbox for aligning the items and making it responsive. I also learned and practiced concepts of designing like color schemes, font-selection, any other... I loved using javascript, as a beginner, I am trying to write more and more javascipt code to learn and practice it. I learned the use of Spread Syntax (...) at some point of my javascript writing as seen below
+```javascript
+const stars=[...document.getElementsByClassName("rate")]
+```
+which I found really useful. And many other similar funtionalites of html, css and javascript I got to know. Looking forward to more such knowledge-acquiring and challenges in the future.
+
+
+### Continued development
+
+I would like to make this interactive rating card component as a template which can be easily integrated with anyone's code and they can get enjoy the funtionalities of it. Also, in future I am looking forward to add transition feature after the pressing of submit button.
+
+
+### Useful resources
+
+- [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference) - This helped me for many reasons. I really liked this website as at any point if I required to know the more functioning of the HTML or CSS tags or attributes, this website helped me.
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- LinkedIn - [LinkedIn Profile](https://www.linkedin.com/in/shivam-kumar-9575a7227/)
+
+
+## Acknowledgments
+
+I would like to thank Frontend Mentor to give me such an amazing idea of creating this website as an challenge. I look forward to see more new and interesting challenges from them in future.
+
--- index.js
+++ index.js
@@ -1,73 +1,68 @@
+const active="rate active"
+const inactive="rate inactive"
+const stars=[...document.getElementsByClassName("rate")]
 let score=[0,0,0,0,0]
-const displayScreen=document.getElementById("selected-score")
-const flipperty=document.getElementById("thankyou-page")
-const flipperrt=document.getElementById("rating-page")
-
+// Listening to ratings
 document.getElementById("s1").addEventListener("click",function(){
     for(let i=0;i<score.length;i++){
         score[i]=0
     }
     score[0]=1
-    document.getElementById("s1").style.backgroundColor="hsl(25, 97%, 53%)"
-    document.getElementById("s1").style.color="white"
-    console.log("1 is clicked")
+    corrector()
 })
 document.getElementById("s2").addEventListener("click",function(){
     for(let i=0;i<score.length;i++){
         score[i]=0
     }
     score[1]=1
-    document.getElementById("s2").style.backgroundColor="hsl(25, 97%, 53%)"
-    document.getElementById("s2").style.color="white"
-    console.log("2 is clicked")
+    corrector()
 })
 document.getElementById("s3").addEventListener("click",function(){
     for(let i=0;i<score.length;i++){
         score[i]=0
     }
     score[2]=1
-    document.getElementById("s3").style.backgroundColor="hsl(25, 97%, 53%)"
-    document.getElementById("s3").style.color="white"
-    console.log("3 is clicked")
+    corrector()
 })
 document.getElementById("s4").addEventListener("click",function(){
     for(let i=0;i<score.length;i++){
         score[i]=0
     }
     score[3]=1
-    document.getElementById("s4").style.backgroundColor="hsl(25, 97%, 53%)"
-    document.getElementById("s4").style.color="white"
-    console.log("4 is clicked")
+    corrector()
 })
 document.getElementById("s5").addEventListener("click",function(){
     for(let i=0;i<score.length;i++){
         score[i]=0
     }
     score[4]=1
-    document.getElementById("s5").style.backgroundColor="hsl(25, 97%, 53%)"
-    document.getElementById("s5").style.color="white"
-    console.log("5 is clicked")
+    corrector()
 })
-document.getElementById("submit-btn").addEventListener("click", function submitted(){
-    let rating = 0
-    for(let i=0;i<score.length;i++){
-        if(score[i]===1){
-            rating=i+1;
-            break;
-        }
-    }
-    console.log(rating)
-    flipperrt.style.display="none"
-    flipperty.style.display="flex"
-    displayScreen.textContent=`You selected ${rating} out of 5`
-
-})
+// Clicked Rating Color Change
 function corrector(){
     for(let i=0;i<score.length;i++){
-        if(score[i]===0){
-            
+        if(score[i]===0)stars[i].className=inactive
+        else stars[i].className=active
+    }
+}
+// Submit Button
+const displayScreen=document.getElementById("selected-score")
+const flipperty=document.getElementById("thankyou-page")
+const flipperrt=document.getElementById("rating-page")
+document.getElementById("submit-btn").addEventListener("click",function(){
+    let rating=-1
+    for(let i=0;i<score.length;i++){
+        if(score[i]===1){
+            rating=i+1
+            break
         }
     }
-    document.querySelector("#thankyou-page").setAttribute("style","background-color:hsl(216, 8%, 25%);color:hsl(217, 12%, 63%);")
-
-}
+    if(rating!=-1){
+        flipperrt.style.display="none"
+        flipperty.style.display="flex"
+        displayScreen.textContent=`You selected ${rating} out of 5`
+    }
+    else{
+        alert("Kindly rate any valid input [1-5]")
+    }
+})
